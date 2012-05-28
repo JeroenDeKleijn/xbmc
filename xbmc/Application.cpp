7fa1cf8227c37c4d132e770bb2d3b5fb54916db4
xbmc <fernetmenta@online.de>
2012-05-28 10:03:31 +0200

    VideoRenerers: add buffering

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -2345,7 +2345,10 @@ void CApplication::Render()
   m_lastFrameTime = XbmcThreads::SystemClockMillis();
 
   if (flip)
+  {
     g_graphicsContext.Flip(dirtyRegions);
+    g_renderManager.NotifyDisplayFlip();
+  }
   CTimeUtils::UpdateFrameTime(flip);
 
   g_TextureManager.FreeUnusedTextures();
