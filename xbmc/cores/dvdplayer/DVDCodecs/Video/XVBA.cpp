e89824afc5d06131f2102e48ca3f1e7b545000da
fritsch <peter.fruehberger@gmail.com>
2012-09-13 23:09:06 +0200

    xvba: satisfy the compiler
    xvba: rethink signed vs unsigned

--- xbmc/cores/dvdplayer/DVDCodecs/Video/XVBA.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/XVBA.cpp
@@ -550,7 +550,7 @@ long CDecoder::Release()
       }
     }
   }
-  IHardwareDecoder::Release();
+  return IHardwareDecoder::Release();
 }
 
 long CDecoder::ReleasePicReference()
@@ -2318,7 +2318,6 @@ void COutput::PreReleaseBufferPool()
 bool COutput::CreateGlxContext()
 {
   GLXContext   glContext;
-  Window       window;
 
   m_Display = g_Windowing.GetDisplay();
   glContext = g_Windowing.GetGlxContext();
